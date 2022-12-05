ba797086ff7d883d91956e482c5c25da779867cc
islam.sanayatov <sanayatov.islam@gmail.com>
2022-12-05 21:11:08 +0600

    init

---
+++ src/main/java/kz/pashim/kubedockertest/KubeDockerTestApplication.java
@@ -0,0 +1,13 @@
+package kz.pashim.kubedockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeDockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeDockerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/pashim/kubedockertest/KubeDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.pashim.kubedockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeDockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
